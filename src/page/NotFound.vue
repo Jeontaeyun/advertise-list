21ebcecb8a2688f67e2706ac3b07206af9cafdc4
Jeontaeyun <jow1813@naver.com>
2019-08-15 17:16:54 +0900

    01 Not Found Page 구현

---
+++ src/page/NotFound.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <p>404</p>
+    <span>Not Found Error</span>
+  </div>
+</template>
+<script>
+import Reply from "../components/Reply";
+export default {
+  components: {
+    Reply
+  },
+  data: function() {
+    return {
+      article: null,
+      replies: []
+    };
+  },
+  created: function() {
+    this.$http
+      .get(`detail.php?req_no=${this.$route.params.id}`)
+      .then(result => {
+        console.log(result.data.detail);
+        this.article = result.data.detail.article;
+        this.replies = result.data.detail.replies;
+      });
+  }
+};
+</script>
+<style lang="less">
+div {
+  margin-top: 6rem;
+  text-align: center;
+  p {
+    font-size: 1rem;
+  }
+  span {
+    font-size: 2rem;
+  }
+}
+</style>
